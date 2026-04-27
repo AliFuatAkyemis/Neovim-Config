5fb201a6371ee7437831a857e709b7f076a5ee66
alifuatakyemis <ali.fuat.akyemis@gmail.com>
2026-04-27 15:44:09 +0300

    fix: disable treesitter for markdown files to resolve nightly parser error

--- init.lua
+++ init.lua
@@ -10,3 +10,12 @@ require("config.lazy")
 
 -- Load keymappings after plugins to ensure dependencies like Telescope are available
 require("config.keymaps")
+
+-- Fix for markdown treesitter error in Neovim nightly
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.treesitter.stop()
+    vim.cmd("syntax enable")
+  end,
+})
