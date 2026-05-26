b5ccdcd2657ad9575e53a25de167f6329f99c8a5
alifuatakyemis <ali.fuat.akyemis@gmail.com>
2026-05-27 00:57:21 +0300

    feat(lint): suppress htmlhint doctype-first in Angular projects under app/

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -9,6 +9,32 @@ return {
         html = { "htmlhint" },
       }
 
+      -- Suppress "doctype-first" error for files under "app/" in Angular projects
+      local htmlhint = lint.linters.htmlhint
+      if htmlhint then
+        local original_parser = htmlhint.parser
+        if original_parser then
+          htmlhint.parser = function(output, bufnr)
+            local diagnostics = original_parser(output, bufnr)
+            local filepath = vim.api.nvim_buf_get_name(bufnr or 0)
+            if filepath ~= "" and filepath:find("[/\\]app[/\\]") then
+              -- Check if this is an Angular project by searching upwards for angular.json or project.json
+              local is_angular = #vim.fs.find({ "angular.json", "project.json" }, { path = filepath, upward = true }) > 0
+              if is_angular then
+                local filtered = {}
+                for _, diag in ipairs(diagnostics) do
+                  if diag.code ~= "doctype-first" then
+                    table.insert(filtered, diag)
+                  end
+                end
+                return filtered
+              end
+            end
+            return diagnostics
+          end
+        end
+      end
+
       -- Create autocommand to run linter
       local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
       vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
@@ -20,3 +46,4 @@ return {
     end,
   }
 }
+
